d2454347661965a366c0501ce598b36022565a0d
devalraj <>
2022-12-28 12:18:14 +0530

    Application v0.1.0

--- api/Redis.js
+++ api/Redis.js
@@ -1,10 +1,23 @@
 const { createClient } = require('redis')
 
 const client = createClient({ url: process.env.REDIS_URL || '' });
-(async () => {
-    await client.connect()
-        .then(() => { console.log("Connected to Rdis"); })
-        .catch(err => { console.log('Redis Client Error', err.message) });
-})()
+var flag = false, eflag = true
+client.connect()
+// .then(() => { console.log("Connected to Rdis"); })
+// .catch(err => { console.log('Redis Client Error', err.message) });
+client.on('connect', () => {
+    if (!flag) {
+        console.log("Connected to Rdis");
+        flag = true;
+        eflag = true;
+    }
+})
+client.on('error', err => {
+    if (eflag) {
+        console.log('Redis Client Error', err.message);
+        flag = false;
+        eflag = false;
+    }
+})
 // await client.disconnect();
 module.exports = client

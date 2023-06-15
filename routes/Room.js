d81597fe805ee3fb6f9566b118a60415ad42f75e
devalraj <vamsi.d@comakeit.com>
2023-06-15 16:57:43 +0530

    Forgot to Remove Unnecessary Code

--- routes/Room.js
+++ routes/Room.js
@@ -8,20 +8,15 @@ const schedule = require('../api/SimpleSchedular');
 router.post('/generate', authenticate, async (req, res) => {
     const id = (crypto.randomBytes(64).toString('hex')).slice(0, 16);
     await client.LPUSH('roomId', id);
-    await client.setEx(id, 60 * 60 * 1000, '1');
     schedule(async () => { await client.LREM('roomId', 1, id); }, 60 * 60 * 1000);
-    return res.send({ "roomid": id, size: 1 });
+    return res.send({ "roomid": id});
 });
 
 router.post('/verify', authenticate, async (req, res) => {
     let ids = await client.LRANGE('roomId', 0, -1);
     if (req.body.roomId !== "" && ids.includes(req.body.roomId))
         return res.sendStatus(201);
-    let size = Number(await client.GET(req.body.roomId));
-    let remaining = await client.TTL(req.body.roomId);
-    size++;
-    await client.SETEX('room', remaining, size.toString());
-    return res.status(401).send({ size });
+    return res.status(401);
 })
 
 module.exports = router
